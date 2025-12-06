5ff407eb873cecf906bc2504e9866240bd5e5bda
Touloupi Maria <pitt.mary2@gmail.com>
2025-12-06 15:17:14 +0200

    adding the proper image view c onnections the art will be updated later

--- demo/src/main/java/com/example/GameScreen.java
+++ demo/src/main/java/com/example/GameScreen.java
@@ -4,14 +4,19 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 public class GameScreen {
     @FXML
@@ -19,8 +24,12 @@ public class GameScreen {
     @FXML
     private Button Run;
     @FXML
-    private Label Label1;
-
+    private RadioButton darkmode;
+    @FXML
+    private ImageView character;
+    @FXML
+    private ImageView partcode;
+    
 
     public void initialize() throws Exception {
 
@@ -34,23 +43,28 @@ public class GameScreen {
 
     String content = code.getText();
 
-    File file = new File("C:/Users/pittm/OneDrive/Desktop/Paper/script.py");
+    File file = new File("C:\\\\Users\\\\pittm\\\\OneDrive\\\\Desktop\\\\Paper\\\\script.py");
 
     try (FileWriter writer = new FileWriter(file)) {
-        //writer.write("");
-       // writer.write(content);
+        writer.write("");
+        writer.write(content);
+        
     }
 
     System.out.println("Saved!");
 
 
 
+                  ProcessBuilder pb = new ProcessBuilder(
+        "C:\\Users\\pittm\\AppData\\Local\\Programs\\Python\\Launcher\\py.exe",
+        "C:\\Users\\pittm\\OneDrive\\Desktop\\Paper\\script.py"
+    );
+              Process process = pb.start();
 
-              ProcessBuilder pb = new ProcessBuilder("python", "script.py");
-        Process process = pb.start();
 
-        BufferedReader reader = new BufferedReader(
-            new InputStreamReader(process.getInputStream())
+
+       BufferedReader reader = new BufferedReader(
+           new InputStreamReader(process.getInputStream())
         );
         BufferedReader errorReader = new BufferedReader(
         new InputStreamReader(process.getErrorStream())
@@ -68,23 +82,11 @@ while ((errorLine = errorReader.readLine()) != null) {
         }
 
         process.waitFor();
+     
 
 
 
 
-    File myObj = new File("labeltest.txt");
-
-    
-    try (Scanner myReader = new Scanner(myObj)) {
-      while (myReader.hasNextLine()) {
-        String data = myReader.nextLine();
-        Label1.setText(data);
-        System.out.println(data);
-      }
-    } catch (FileNotFoundException e) {
-      System.out.println("An error occurred.");
-      e.printStackTrace();
-    }
 
 
     
@@ -94,10 +96,22 @@ while ((errorLine = errorReader.readLine()) != null) {
     }
 
 
+    @FXML
+    private void onDarkmodeToggled() {
+        System.out.println("Darkmode toggled!");
+    if (darkmode.isSelected()) {
+        code.getStylesheets().add(getClass().getResource("/com/example/css/darkmode.css").toExternalForm());
+        code.getStyleClass().add("code");
+        Image image = new Image("file:/C:/Users/pittm/OneDrive/Desktop/Paper/coding_game/demo/src/main/resources/com/example/dark.png");
+partcode.setImage(image);
 
+    } else {
+        code.getStylesheets().clear();
+        Image image = new Image("file:/C:/Users/pittm/OneDrive/Desktop/Paper/coding_game/demo/src/main/resources/com/example/white.png");
+partcode.setImage(image);
+    }
 
-
-
+}
 
 
 
